baf33b1360e95a2abb41021ae911613b414a19ac
Koi-3088 <koi.github3088@gmail.com>
2020-09-16 19:25:55 -0500

    Rework EggRoll cooldown (static list in favor of a txt file). Start clean up and refactor

--- SysBot.Pokemon.Discord/Commands/Bots/TradeModule.cs
+++ SysBot.Pokemon.Discord/Commands/Bots/TradeModule.cs
@@ -77,9 +77,8 @@ namespace SysBot.Pokemon.Discord
         {
             const int gen = 8;
             content = ReusableActions.StripCodeBlock(content);
-            SpecifyOT(content, out string specifyOT);
-            if (specifyOT != string.Empty)
-                content = System.Text.RegularExpressions.Regex.Replace(content, @"OT:(\S*\s?\S*\s?\S*)?$\W?", "", System.Text.RegularExpressions.RegexOptions.Multiline);
+            TradeExtensions.SpecifyOT(content, out string specifyOT);
+            content = !specifyOT.Equals(string.Empty) ? System.Text.RegularExpressions.Regex.Replace(content, @"OT:(.*)?$\W?", "", System.Text.RegularExpressions.RegexOptions.Multiline) : content;
 
             var set = new ShowdownSet(content);
             var template = AutoLegalityWrapper.GetTemplate(set);
@@ -93,8 +92,7 @@ namespace SysBot.Pokemon.Discord
 
             var sav = AutoLegalityWrapper.GetTrainerInfo(gen);
             var pkm = sav.GetLegal(template, out _);
-            if (specifyOT != string.Empty)
-                pkm.OT_Name = specifyOT;
+            pkm.OT_Name = !specifyOT.Equals(string.Empty) ? specifyOT : pkm.OT_Name;
 
             var la = new LegalityAnalysis(pkm);
             var spec = GameInfo.Strings.Species[template.Species];
@@ -151,117 +149,63 @@ namespace SysBot.Pokemon.Discord
             if (Info.Hub.Config.Trade.EggRollCooldown < 0)
                 Info.Hub.Config.Trade.EggRollCooldown = default;
 
-            if (!System.IO.File.Exists("EggRollCooldown.txt"))
-                System.IO.File.Create("EggRollCooldown.txt").Close();
-
-            System.IO.StreamReader reader = new System.IO.StreamReader("EggRollCooldown.txt");
-            var content = reader.ReadToEnd();
-            reader.Close();
-
-            var id = $"{Context.User.Id}";
-            var parse = System.Text.RegularExpressions.Regex.Match(content, @"(" + id + @") - (.*$)", System.Text.RegularExpressions.RegexOptions.Multiline);
-            System.DateTime.TryParse(parse.Groups[2].Value, out System.DateTime time);
+            var id = Context.User.Id.ToString();
+            var line = TradeExtensions.EggRollCooldown.FirstOrDefault(z => z.Contains(id));
+            System.DateTime.TryParse(line != null ? line.Split(',')[1] : string.Empty, out System.DateTime time);
             var timer = time.AddHours(Info.Hub.Config.Trade.EggRollCooldown);
             var timeRemaining = timer - System.DateTime.Now;
 
-            if (content.Contains(id) && System.DateTime.Now < timer)
+            if (System.DateTime.Now < timer)
             {
                 await ReplyAsync($"{Context.User.Mention}, please try again in {timeRemaining.Hours:N0}h : {timeRemaining.Minutes:N0}m : {timeRemaining.Seconds:N0}s!").ConfigureAwait(false);
                 return;
             }
 
             var code = Info.GetRandomTradeCode();
-            int[] validEgg = 
-                { 1, 4, 7, 10, 27, 37, 43, 50, 52, 54, 58, 60, 63, 66, 72,
-                  77, 79, 81, 83, 90, 92, 95, 98, 102, 104, 108, 109, 111, 114, 115,
-                  116, 118, 120, 122, 123, 127, 128, 129, 131, 133, 137, 163, 170, 172, 173,
-                  174, 175, 177, 194, 206, 211, 213, 214, 215, 220, 222, 223, 225, 227, 236,
-                  241, 246, 263, 270, 273, 278, 280, 290, 293, 298, 302, 303, 309, 318, 320,
-                  324, 328, 337, 338, 339, 341, 343, 349, 355, 360, 361, 403, 406, 415, 420,
-                  422, 425, 427, 434, 436, 438, 439, 440, 446, 447, 449, 451, 453, 458, 459,
-                  479, 506, 509, 517, 519, 524, 527, 529, 532, 535, 538, 539, 543, 546, 548,
-                  550, 551, 554, 556, 557, 559, 561, 562, 568, 570, 572, 574, 577, 582, 587,
-                  588, 590, 592, 595, 597, 599, 605, 607, 610, 613, 616, 618, 619, 621, 622,
-                  624, 626, 627, 629, 631, 632, 633, 636, 659, 661, 674, 677, 679, 682, 684,
-                  686, 688, 690, 692, 694, 701, 702, 704, 707, 708, 710, 712, 714, 722, 725,
-                  728, 736, 742, 744, 746, 747, 749, 751, 753, 755, 757, 759, 761, 764, 765,
-                  766, 767, 769, 771, 776, 777, 778, 780, 781, 782, 810, 813, 816, 819, 821,
-                  824, 827, 829, 831, 833, 835, 837, 840, 843, 845, 846, 848, 850, 852, 854,
-                  856, 859, 868, 870, 871, 872, 874, 875, 876, 877, 878, 884, 885 };
-
-            int[] regional = { 27, 37, 50, 52, 77, 79, 83, 122, 222, 263, 554, 562, 618 };
-
-            int[] shinyOdds = { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-                                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-                                3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-                                5, 5, 5, 5, 5, 5, 5, 6, 6, 6};
-
-            int[] abilityIndex = { 0, 1, 2 };
-            int[] formIndex1 = { 0, 1 };
-            int[] formIndex2 = { 0, 1, 2 };
-
             var rng = new System.Random();
-            int shinyRng = rng.Next(0, shinyOdds.Length);
-            int abilityRng = rng.Next(0, abilityIndex.Length);
-            var set = new ShowdownSet($"Egg({SpeciesName.GetSpeciesName(rng.Next(new Zukan8Index(Zukan8Type.None, 1).Index, GameUtil.GetMaxSpeciesID(GameVersion.SWSH)), 2)})");
-            while (!validEgg.ToList().Contains(set.Species))
-                set = new ShowdownSet($"Egg({SpeciesName.GetSpeciesName(rng.Next(new Zukan8Index(Zukan8Type.None, 1).Index, GameUtil.GetMaxSpeciesID(GameVersion.SWSH)), 2)})");
-
+            int shinyRng = rng.Next(0, TradeExtensions.shinyOdds.Length);
+            int abilityRng = rng.Next(0, TradeExtensions.abilityIndex.Length);
+            var set = new ShowdownSet($"Egg({SpeciesName.GetSpeciesName((int)TradeExtensions.validEgg.GetValue(rng.Next(0, TradeExtensions.validEgg.Length)), 2)})");
             var template = AutoLegalityWrapper.GetTemplate(set);
             var sav = AutoLegalityWrapper.GetTrainerInfo(8);
             var pkm = (PK8)sav.GetLegal(template, out _);
 
-            if (regional.ToList().Contains(pkm.Species))
+            if (TradeExtensions.regional.ToList().Contains(pkm.Species))
             {
-                int formRng = rng.Next(0, formIndex1.Length);
-                int formRng2 = rng.Next(0, formIndex2.Length);
+                int formRng = rng.Next(0, TradeExtensions.formIndex1.Length);
+                int formRng2 = rng.Next(0, TradeExtensions.formIndex2.Length);
 
                 if (pkm.Species != 52)
-                    pkm.SetAltForm(formIndex1[formRng]);
-                else pkm.SetAltForm(formIndex2[formRng2]);
+                    pkm.SetAltForm(TradeExtensions.formIndex1[formRng]);
+                else pkm.SetAltForm(TradeExtensions.formIndex2[formRng2]);
 
                 if (pkm.AltForm != 0)
                 {
-                    if (pkm.Species == 27)
-                        pkm.RelearnMove3 = 10;
-                    else if (pkm.Species == 37)
-                        pkm.RelearnMove4 = 39;
-                    else if (pkm.Species == 52)
+                    switch (pkm.Species)
                     {
-                        pkm.RelearnMove2 = 252;
-                        pkm.RelearnMove3 = 45;
-                    }
-                    else if (pkm.Species == 83)
-                    {
-                        pkm.RelearnMove1 = 64;
-                        pkm.RelearnMove4 = 28;
-                    }
-                    else if (pkm.Species == 222)
-                        pkm.RelearnMove1 = 33;
-                    else if (pkm.Species == 263)
-                    {
-                        pkm.RelearnMove2 = 43;
-                        pkm.RelearnMove3 = 0;
-                        pkm.RelearnMove4 = 0;
-                    }
+                        case 27: pkm.RelearnMove3 = 10; break;
+                        case 37: pkm.RelearnMove4 = 39; break;
+                        case 52: pkm.RelearnMove2 = 252; pkm.RelearnMove3 = 45; break;
+                        case 83: pkm.RelearnMove1 = 64; pkm.RelearnMove4 = 28; break;
+                        case 222: pkm.RelearnMove1 = 33; break;
+                        case 263: pkm.RelearnMove2 = 43; pkm.RelearnMove3 = 0; pkm.RelearnMove4 = 0; break;
+                    };
                 }
             }
 
-            EggTrade(pkm);
+            TradeExtensions.EggTrade(pkm);
             pkm.Nature = rng.Next(0, 24);
             pkm.StatNature = pkm.Nature;
-            pkm.SetAbilityIndex(abilityIndex[abilityRng]);
+            pkm.SetAbilityIndex(TradeExtensions.abilityIndex[abilityRng]);
             pkm.IVs = pkm.SetRandomIVs(3);
             BallApplicator.ApplyBallLegalRandom(pkm);
 
-            if (shinyOdds[shinyRng] == 3)
+            switch (TradeExtensions.shinyOdds[shinyRng])
             {
-                CommonEdits.SetShiny(pkm, Shiny.Never);
-                pkm.SetUnshiny();
-            }
-            else if (shinyOdds[shinyRng] == 5)
-                CommonEdits.SetShiny(pkm, Shiny.AlwaysStar);
-            else CommonEdits.SetShiny(pkm, Shiny.AlwaysSquare);
+                case 3: CommonEdits.SetShiny(pkm, Shiny.Never); pkm.SetUnshiny(); break;
+                case 5: CommonEdits.SetShiny(pkm, Shiny.AlwaysStar); break;
+                case 6: CommonEdits.SetShiny(pkm, Shiny.AlwaysSquare); break;
+            };
 
             var la = new LegalityAnalysis(pkm);
             var spec = GameInfo.Strings.Species[template.Species];
@@ -280,17 +224,18 @@ namespace SysBot.Pokemon.Discord
 
         private async Task AddTradeToQueueAsync(int code, string trainerName, PK8 pk8, RequestSignificance sig)
         {
-            if (!pk8.CanBeTraded() || !IsItemMule(pk8))
+            if (!pk8.CanBeTraded() || !TradeExtensions.IsItemMule(pk8))
             {
-                await ReplyAsync($"{(Info.Hub.Config.Trade.ItemMuleCustomMessage == string.Empty || IsItemMule(pk8) ? "Provided Pokémon content is blocked from trading!" : Info.Hub.Config.Trade.ItemMuleCustomMessage)}").ConfigureAwait(false);
+                var msg = "Provided Pokémon content is blocked from trading!";
+                await ReplyAsync($"{(!Info.Hub.Config.Trade.ItemMuleCustomMessage.Equals(string.Empty) && !Info.Hub.Config.Trade.ItemMuleSpecies.Equals(Species.None) ? Info.Hub.Config.Trade.ItemMuleCustomMessage : msg)}").ConfigureAwait(false);
                 return;
             }
 
             if (Info.Hub.Config.Trade.DittoTrade && pk8.Species == 132)
-                DittoTrade(pk8);
+                TradeExtensions.DittoTrade(pk8);
 
             if (Info.Hub.Config.Trade.EggTrade && pk8.Nickname == "Egg")
-                EggTrade(pk8);
+                TradeExtensions.EggTrade(pk8);
 
             var la = new LegalityAnalysis(pk8);
             if (!la.Valid && SysCordInstance.Self.Hub.Config.Legality.VerifyLegality)
@@ -300,13 +245,6 @@ namespace SysBot.Pokemon.Discord
             }
 
             await Context.AddToQueueAsync(code, trainerName, sig, pk8, PokeRoutineType.LinkTrade, PokeTradeType.Specific).ConfigureAwait(false);
-        }
-
-        private bool IsItemMule(PK8 pk8)
-        {
-            if (Info.Hub.Config.Trade.ItemMuleSpecies == Species.None || Info.Hub.Config.Trade.DittoTrade && pk8.Species == 132 || Info.Hub.Config.Trade.EggTrade && pk8.Nickname == "Egg")
-                return true;
-            return !(pk8.Species != SpeciesName.GetSpeciesID(Info.Hub.Config.Trade.ItemMuleSpecies.ToString()) || pk8.IsShiny);
         }
 
         private async Task<bool> TrollAsync(bool invalid, IBattleTemplate set)
@@ -327,85 +265,6 @@ namespace SysBot.Pokemon.Discord
                 return true;
             }
             return false;
-        }
-
-        public static void DittoTrade(PKM pk8)
-        {
-            if (pk8.IsNicknamed == false)
-                return;
-
-            var dittoLang = new string[] { "JPN", "ENG", "FRE", "ITA", "GER", "ESP", "KOR", "CHS", "CHT" };
-            var dittoStats = new string[] { "ATK", "SPE", "SPA" };
-
-            if (pk8.Nickname.Contains(dittoLang[0]))
-                pk8.Language = (int)LanguageID.Japanese;
-            else if (pk8.Nickname.Contains(dittoLang[1]))
-                pk8.Language = (int)LanguageID.English;
-            else if (pk8.Nickname.Contains(dittoLang[2]))
-                pk8.Language = (int)LanguageID.French;
-            else if (pk8.Nickname.Contains(dittoLang[3]))
-                pk8.Language = (int)LanguageID.Italian;
-            else if (pk8.Nickname.Contains(dittoLang[4]))
-                pk8.Language = (int)LanguageID.German;
-            else if (pk8.Nickname.Contains(dittoLang[5]))
-                pk8.Language = (int)LanguageID.Spanish;
-            else if (pk8.Nickname.Contains(dittoLang[6]))
-                pk8.Language = (int)LanguageID.Korean;
-            else if (pk8.Nickname.Contains(dittoLang[7]))
-                pk8.Language = (int)LanguageID.ChineseS;
-            else if (pk8.Nickname.Contains(dittoLang[8]))
-                pk8.Language = (int)LanguageID.ChineseT;
-
-            pk8.MetDate = System.DateTime.Parse("2020/10/20");
-            pk8.StatNature = pk8.Nature;
-            pk8.SetAbility(7);
-            pk8.SetAbilityIndex(1);
-            pk8.Met_Level = 60;
-            pk8.Move1 = 144;
-            pk8.Move1_PP = 0;
-            pk8.Met_Location = 154;
-            pk8.Ball = 21;
-            pk8.IVs = new int[] { 31, pk8.Nickname.Contains(dittoStats[0]) ? 0 : 31, 31, pk8.Nickname.Contains(dittoStats[1]) ? 0 : 31, pk8.Nickname.Contains(dittoStats[2]) ? 0 : 31, 31 };
-            pk8.SetSuggestedHyperTrainingData();
-        }
-
-        public static void EggTrade(PK8 pk8)
-        {
-            pk8.IsEgg = true;
-            pk8.Egg_Location = 60002;
-            pk8.EggMetDate = System.DateTime.Parse("2020/10/20");
-            pk8.HeldItem = 0;
-            pk8.CurrentLevel = 1;
-            pk8.EXP = 0;
-            pk8.DynamaxLevel = 0;
-            pk8.Met_Level = 1;
-            pk8.Met_Location = 0;
-            pk8.MetDate = System.DateTime.Parse("2020/10/20");
-            pk8.CurrentHandler = 0;
-            pk8.OT_Friendship = 1;
-            pk8.HT_Name = "";
-            pk8.HT_Friendship = 0;
-            pk8.HT_Language = 0;
-            pk8.HT_Gender = 0;
-            pk8.HT_Memory = 0;
-            pk8.HT_Feeling = 0;
-            pk8.HT_Intensity = 0;
-            pk8.EVs = new int[] { 0, 0, 0, 0, 0, 0 };
-            pk8.Markings = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-            pk8.ClearRecordFlags();
-            pk8.GetSuggestedRelearnMoves();
-            pk8.Moves = pk8.RelearnMoves;
-            pk8.Move1_PPUps = pk8.Move2_PPUps = pk8.Move3_PPUps = pk8.Move4_PPUps = 0;
-            pk8.SetMaximumPPCurrent(pk8.Moves);
-            pk8.SetSuggestedHyperTrainingData();
-        }
-
-        public static string SpecifyOT(string content, out string specifyOT)
-        {
-            if (!content.Contains("OT: "))
-                return specifyOT = string.Empty;
-
-            return specifyOT = System.Text.RegularExpressions.Regex.Match(content, @"OT:(.*)?$\W?", System.Text.RegularExpressions.RegexOptions.Multiline).Groups[1].Value.Trim();
         }
     }
 }
